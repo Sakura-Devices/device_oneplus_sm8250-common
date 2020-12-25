db6ebe774a9a8fca45b69ac311a826b127a96352
Jack <jacksummers715@gmail.com>
2020-12-25 02:47:16 +0100

    add the path to the permission
    - this is temp fix as a test; soon  adding to device tree so it doesnt have to rely on the permission to be present in the vendor files
    
    Change-Id: Iee76ed036a191757a38de5136d3675ee6768808f

--- common.mk
+++ common.mk
@@ -46,7 +46,8 @@ PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # A/B
 AB_OTA_UPDATER := true
